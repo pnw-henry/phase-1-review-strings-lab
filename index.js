5a7cf265131a9b7c35ad1aaa6d1e58c11389c927
Henry Escobar <henrye@gmail.com>
2022-05-17 15:38:47 -0700

    commit tree 9e310c53fb9692801afbb858de9456810213cfb6

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = 'Henry';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}, ${currentUser.toUpperCase()}!`;
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
